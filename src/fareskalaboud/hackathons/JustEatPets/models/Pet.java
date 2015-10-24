2ce1429b0e3b05599be7f7526a1ac246898f01c6
Fares Alaboud <fares.alaboud@gmail.com>
2015-10-24 21:05:22 +0100

    initial commit, model classes done

---
+++ src/fareskalaboud/hackathons/JustEatPets/models/Pet.java
@@ -0,0 +1,9 @@
+package fareskalaboud.hackathons.JustEatPets.models;
+
+/**
+ * Created by faresalaboud on 24/10/2015.
+ */
+public enum Pet {
+    CAT, DOG, FISH
+
+}
---
+++ src/fareskalaboud/hackathons/JustEatPets/models/Restaurant.java
@@ -0,0 +1,37 @@
+package fareskalaboud.hackathons.JustEatPets.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by faresalaboud on 24/10/2015.
+ */
+public class Restaurant {
+
+    private int id;
+    private String name;
+    private String postcode;
+    private ArrayList<Product> products;
+
+    public Restaurant(int id, String name, String postcode) {
+        this.id = id;
+        this.name = name;
+        this.postcode = postcode;
+        products = new ArrayList<Product>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+}

feb959fbdeae7a6a3c2c7e458a85d31165cc8081
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-25 01:18:41 +0100

    Added logo to top of app

--- app/src/main/java/jkfj/brumhack/justeatpetsapp/MainActivity.java
+++ app/src/main/java/jkfj/brumhack/justeatpetsapp/MainActivity.java
@@ -2,12 +2,23 @@ package jkfj.brumhack.justeatpetsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnShootPet;
+    private Button btnCat;
+    private Button btnDog;
+    private Button btnFish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShootPet = (Button) findViewById(R.id.btnShootPet);
+        btnCat = (Button) findViewById(R.id.btnCat);
+        btnDog = (Button) findViewById(R.id.btnDog);
+        btnFish = (Button) findViewById(R.id.btnFish);
     }
 }
